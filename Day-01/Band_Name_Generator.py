fc563d5e16bebe10f63b9f34a4d95cb80582d348
Samir kumar shaw <samir.kshaw2005@gmail.com>
2026-03-21 08:12:38 +0530

    Day 01 - Band Name Generator

---
+++ Day-01/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator")
+city = input("what is the name of your city\n")
+pet = input("what is the name of your pet\n")
+print("Your band name could be:" + city  +"_" + pet )
